53f1459267f1c2832f1ee2e26319585e780ccb41
Nik Divjak <root1@Niks-MacBook-Pro.local>
2023-01-18 00:27:24 +0100

    Added radix src file, still need to write the logic

--- src/main.c
+++ src/main.c
@@ -5,9 +5,16 @@ int	main(int ac, char **av)
 	t_list	*stack;
 
 	ft_printf("\nStarting push_swap:\n");
-	// stack = lst_create_stack(ac, av);
-	stack = debug_create_stack(0, 25, 5);
-	debug_push_stacks(stack, NULL, 3);
-	// debug_print_stack(stack);
+	stack = lst_create_stack(ac, av);
+	debug_print_stack(stack);
 	return (0);
 }
+
+// Main for Debugging
+// int	main(void)
+// {
+// 	ft_printf("\nStarting push_swap in debug mode:\n");
+// 	stack = debug_create_stack(0, 25, 5);
+// 	debug_push_stacks(stack, NULL, 3);
+// 	return (0);
+// }
---
+++ src/radix.c
@@ -0,0 +1,5 @@
+#include "../push_swap.h"
+
+void	radix_controller(t_start_pos *stack_pos)
+{
+}
